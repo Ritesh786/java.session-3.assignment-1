721f27d8a8f5f229f79d5ecfa2bd6efa3f34a525
Ritesh786 <jharitesh6@gmail.com>
2016-07-17 18:12:57 +0530

    session3,assignment 1
    session3,assignment 1

---
+++ Root.java
@@ -0,0 +1,17 @@
+
+public class Root {
+	 static  void fun(){
+		 System.out.println(gun());
+		 System.out.println(sun());
+		 
+	 }
+	 static double gun(){
+		 return Math.sqrt(16);
+		 
+	 }
+	 static double sun(){
+		 return Math.cbrt(64*8);
+		 
+	 }
+
+}
---
+++ SqreMat.java
@@ -0,0 +1,9 @@
+
+public class SqreMat {
+
+	public static void main(String[] args) {
+		Root.fun();
+
+	}
+
+}
